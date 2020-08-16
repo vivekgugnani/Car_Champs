8e768ebaa14059be56608007e70a1c31e1c528e9
Vivek <vivek0094.cse19@chitkara.edu.in>
2020-08-16 22:41:32 +0530

    made car to change color

--- javaScript/service.js
+++ javaScript/service.js
@@ -1,28 +1,26 @@
-function Display(String id){
-    String choice = id;
-    var img = document.getElementsByClassName(id)
-    switch(choice){
-        case "img1":
-            img.src= "Images/WhiteAudi.webp";
-            img.style.display= "flex";
-            break;
-        case "img2":
-            img.src= "Images/BlackAudi.webp";
-            img.style.display= "flex";
-            break;
-        case "img3":
-            img.src= "Images/greyAudi.webp";
-            img.style.display= "flex";
-            break;
-        case "img4":
-            img.src= "Images/SevellicAudi.webp";
-            img.style.display= "flex";
-            break;
-        case "img5":
-            img.src= "Images/FirmanentblueAudi.webp.webp";
-            img.style.display= "flex";
-            break;    
-
-
-    }
+function Display( id) {
+  var choice = id;
+  var img = document.getElementsByClassName(id);
+  switch (choice) {
+    case "img1":{
+      img.src = "Images/WhiteAudi.webp";
+      img.style.display = "flex";
+      break;}
+    case "img2":{
+      img.src = "Images/BlackAudi.webp";
+      img.style.display = "flex";
+      break;}
+    case "img3":{
+      img.src = "Images/greyAudi.webp";
+      img.style.display = "flex";
+      break;}
+    case "img4":{
+      img.src = "Images/SevellicAudi.webp";
+      img.style.display = "flex";
+      break;}
+    case "img5":{
+      img.src = "Images/FirmanentblueAudi.webp.webp";
+      img.style.display = "flex";
+      break;}
+  }
 }
